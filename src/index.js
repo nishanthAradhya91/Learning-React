478b29be37fa169a53f3398618cab3a4ddfd2887
nishantharadhya_ms <Nishnatharadhya.MS@Ltts.com>
2019-12-10 11:09:15 +0530

    'session68'

--- src/index.js
+++ src/index.js
@@ -1,23 +1,56 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import Comment from './CommentDetail'
-import ApprovalCard from './ApprovalCard'
+import { nullLiteral } from '@babel/types';
+import SeasonDisplay from './SeasonDisplay.js';
 
-const App=()=>{
+// import Comment from './CommentDetail'
+// import ApprovalCard from './ApprovalCard'
+
+// const App=()=>{
+//     window.navigator.geolocation.getCurrentPosition(
+//         (position)=>console.log(position),
+//         (err)=>console.log(err)
+//     );
+//     return(
+//         // <div className="ui container comments">
+//         //     <ApprovalCard>
+//         //    <Comment author='Sam'/> 
+//         //    </ApprovalCard>  
+//         //    <ApprovalCard>
+//         //    <Comment author='Jan'/>
+//         //    <ApprovalCard>
+//         //    </ApprovalCard>
+//         //    <Comment author='Fish'/>
+//         //    </ApprovalCard>  
+//         // </div>
+        
+//         <div>Lattitude:</div>
+//         );
+// }
+
+
+class App extends React.Component{
    
-    return(
-        <div className="ui container comments">
-            <ApprovalCard>
-           <Comment author='Sam'/> 
-           </ApprovalCard>  
-           <ApprovalCard>
-           <Comment author='Jan'/>
-           <ApprovalCard>
-           </ApprovalCard>
-           <Comment author='Fish'/>
-           </ApprovalCard>  
-        </div>
+    state = {lat:nullLiteral,errorMessage:''}
+
+    componentDidMount(){
+        window.navigator.geolocation.getCurrentPosition(
+            (position)=>this.setState({lat:position.coords.latitude}),
+            (err)=>this.setState({errorMessage:err.message})
         );
+    }
+    render(){
+        
+       if(this.state.errorMessage && !this.state.late){
+           return <div>Error:{this.state.errorMessage}</div>
+       }
+       if(!this.state.errorMessage && this.state.lat){
+        return <SeasonDisplay lat={this.state.lat} />
+        return
+       }
+       return <div>loading!</div>
+    }
+    
 }
 
 ReactDom.render(
